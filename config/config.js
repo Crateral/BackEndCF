28d8470ffc33dcfca7cb8160f0fb35ca2139ca98
Crateral <49699153+Crateral@users.noreply.github.com>
2024-05-03 18:33:32 -0500

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 module.exports.SEED = 'este-es-un-seed-dificil';
 module.exports.CORREO = 'cesar.a.hidalgo.g@gmail.com';
-module.exports.PASSWORD = 'Correo1234*;
+module.exports.PASSWORD = 'Correo1234*';
 module.exports.ASUNTO = 'Inscripcion Bengala Fit Club';
 module.exports.MENSAJE = 'Bienvenido, ahora eres parte de Bengala Fit Club';
